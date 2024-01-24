31e3eda794cb5cde084ab62c9a3543ab436c7262
daat <tempest1996@gmail.com>
2024-01-24 14:39:00 +0900

    변경

---
+++ pythonProject/Day_0124/ex_high_tem2.py
@@ -0,0 +1,28 @@
+import csv
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+
+def draw_graph_on_date(month, day):
+    f = open('../대구기온2-utf8.csv',encoding='utf-8-sig')
+    data = csv.reader(f)
+    next(data)
+    result = []
+
+    for row in data:
+        if row[-1] != '':
+            date_string = row[0].split('-')
+            if date_string[1] == month and date_string[2] == day:
+                result.append(float(row[-1]))
+
+    f.close()
+    plt.figure(figsize=(15,2))
+    plt.plot(result, 'royalblue')
+    plt.rc('axes', unicode_minus=False)
+    plt.rc('font', family='Malgun Gothic')
+    plt.title(f'매년{month}월 {day}일 최고 기온 변화')
+    plt.show()
+
+
+month, day = input('날짜(월 일)를 입력하세요: ').split()
+draw_graph_on_date(month, day)
+
